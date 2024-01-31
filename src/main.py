ad48aeb7d775032a37c0a7017a8653c6184905ef
Elliot Scher <ecscher84@gmail.com>
2024-01-31 08:00:26 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Elliot Scher                                                 #
+# 	Created:      1/31/2024, 7:59:50 AM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
